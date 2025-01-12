9146de414ad04561fd9ed9ab5d17c8c19c375349
MaxM1567 <metsomax@mail.ru>
2025-01-12 08:24:12 +0300

    Добавил базовый интерфейс и возможность и логику для отправки и вывода запросов

--- app/src/main/java/com/example/bininfo/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/bininfo/fragments/SearchFragment.kt
@@ -1,18 +1,39 @@
 package com.example.bininfo.fragments
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.bininfo.R
+import com.example.bininfo.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    private var searchBinding: FragmentSearchBinding? = null
+    private val binding get() = searchBinding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+
+        searchBinding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.editText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+
+                Toast.makeText(context, binding.editText.text, Toast.LENGTH_SHORT).show()
+
+                return@OnKeyListener true
+            }
+            false
+        })
     }
 }
